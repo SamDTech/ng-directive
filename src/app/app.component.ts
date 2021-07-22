041306ebeb7a1880b57b27664767b52e20a15e9d
samdtech <opeyemisamuel222@gmail.com>
2021-07-22 12:37:42 +0100

    ng- directives with custom     <ng-container *appTimes='images.length; let i = '>     <ng-container *appTimes='images.length; let i = '>     <ng-container *appTimes='images.length; let i = '>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,114 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-pages';
+  currentPage: number = 0;
+
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1619537905033-fb5406e5a1aa?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Summer',
+      url: 'https://images.unsplash.com/photo-1622495966349-2857f74777a0?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8c3VtbWVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Winter life',
+      url: 'https://images.unsplash.com/photo-1604537466608-109fa2f16c3b?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8d2ludGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Forest camp',
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1619537905033-fb5406e5a1aa?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Summer',
+      url: 'https://images.unsplash.com/photo-1622495966349-2857f74777a0?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8c3VtbWVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Winter life',
+      url: 'https://images.unsplash.com/photo-1604537466608-109fa2f16c3b?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8d2ludGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Forest camp',
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1619537905033-fb5406e5a1aa?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Summer',
+      url: 'https://images.unsplash.com/photo-1622495966349-2857f74777a0?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8c3VtbWVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Winter life',
+      url: 'https://images.unsplash.com/photo-1604537466608-109fa2f16c3b?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8d2ludGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Forest camp',
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1619537905033-fb5406e5a1aa?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Summer',
+      url: 'https://images.unsplash.com/photo-1622495966349-2857f74777a0?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8c3VtbWVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Winter life',
+      url: 'https://images.unsplash.com/photo-1604537466608-109fa2f16c3b?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8d2ludGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Forest camp',
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1619537905033-fb5406e5a1aa?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Summer',
+      url: 'https://images.unsplash.com/photo-1622495966349-2857f74777a0?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8c3VtbWVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Winter life',
+      url: 'https://images.unsplash.com/photo-1604537466608-109fa2f16c3b?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8d2ludGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+
+    {
+      title: 'Forest camp',
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
+
+  updatePage(i: number) {
+    this.currentPage = i;
+  }
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
